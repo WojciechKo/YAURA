a6e9ad4cecf45d3843f92302b7110fe771fa179c
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-07-12 11:01:45 +0200

    Simple grid

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,14 +5,16 @@ import './styles.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="header">
+          Header
+        </div>
+        <div className="main">
+          Content
+        </div>
+        <div className="footer">
+          Footer
+        </div>
       </div>
     );
   }
