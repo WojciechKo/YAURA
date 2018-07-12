482ca2e1abc68f4c7d1e97a81cb4a71c99d225e1
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-07-12 11:14:34 +0200

    Full screen

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,7 +10,11 @@ class App extends Component {
           Header
         </div>
         <div className="content">
-          Content
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat ultricies dui, quis euismod ante imperdiet ac. Fusce et aliquet sem. Suspendisse tincidunt neque nec bibendum tempus. Nulla commodo, quam at consequat porta, ante ex dapibus eros, pellentesque vestibulum ipsum diam vel erat. Nam porttitor sed nibh eu pellentesque. Pellentesque diam elit, bibendum non enim sit amet, finibus laoreet urna. Donec a dolor bibendum, porta diam a, tempus sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque faucibus felis risus, a congue nisi scelerisque et. Aliquam lacus urna, sollicitudin pharetra interdum at, dictum in diam. Nunc nec egestas neque, non egestas risus.
+
+Proin sem diam, blandit ac mauris in, vehicula ullamcorper tellus. Suspendisse ut varius orci. Donec volutpat nulla non hendrerit commodo. Pellentesque facilisis sagittis velit. Aliquam erat volutpat. Pellentesque vel massa rhoncus, viverra velit sit amet, ultrices ex. Nam gravida lacus et ante lobortis porttitor. Nunc vitae enim pulvinar, dictum purus eu, vulputate dolor. Aenean sagittis scelerisque nulla, ut vehicula libero cursus ac. Nam ultricies tortor tempus tincidunt lobortis. Duis nec odio sed nisl laoreet sagittis.
+
+Praesent a risus ornare, blandit sem eu, fermentum nisi. Integer dignissim purus vel felis dictum venenatis. In vel sollicitudin arcu, ut varius elit. Integer dictum purus purus, vel imperdiet ex vehicula id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec eu venenatis neque, vitae volutpat ex. Proin maximus tincidunt orci ut varius. Donec vehicula auctor elementum. Maecenas consequat luctus finibus. Maecenas non risus ac arcu tristique pellentesque et ac diam.
         </div>
         <div className="footer">
           Footer
