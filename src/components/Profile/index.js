0fdc08070bacb68f6ba7cd29b9458882b9e721c5
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-07-12 19:49:13 +0200

    Import web3

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
+import Web3 from 'web3';
+
 class Profile extends Component {
   render() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    console.log(web3);
+    web3.eth.getAccounts().then(console.log);
+
     return (
       <div>
         User profile!!!
