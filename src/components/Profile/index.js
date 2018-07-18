3832614b9f72ca62c43658d5f546e11517db143c
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-07-18 20:48:11 +0200

    Extract OwnerInfo

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import PropTypes from 'prop-types';
+
+import OwnerInfo from './ownerInfo.js';
 
 class Profile extends Component {
   static propTypes = {
@@ -7,33 +9,13 @@ class Profile extends Component {
     bethOwner: PropTypes.string
   };
 
-  state = {
-    newBethOwner: ''
-  }
-
-  onOwnerIdChange = (event) => {
-    this.setState({
-      newBethOwner: event.target.value
-    });
-  }
-
-  componentWillReceiveProps = (nextProps) => {
-    this.setState({
-      newBethOwner: nextProps.bethOwner
-    });
-  }
-
   render() {
     return (
       <div>
         <section>User</section>
         <article>{this.props.walletId}</article>
 
-        <section>Update owner of contract</section>
-        <article>
-          Current owner hash: { this.props.bethOwner }
-          <input type="text" value={this.state.newBethOwner} onChange={this.onOwnerIdChange}/>
-        </article>
+        <OwnerInfo bethOwner={this.props.bethOwner}/>
       </div>
     );
   }
---
+++ src/components/Profile/ownerInfo.js
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+class OwnerInfo extends Component {
+  static propTypes = {
+    bethOwner: PropTypes.string
+  };
+
+  state = {
+    newBethOwner: ''
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({
+      newBethOwner: nextProps.bethOwner
+    });
+  }
+
+  onOwnerIdChange = (event) => {
+    this.setState({
+      newBethOwner: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <section>Update owner of contract</section>
+        <article>
+          Current owner hash: { this.props.bethOwner }
+          <input type="text" value={this.state.newBethOwner} onChange={this.onOwnerIdChange}/>
+        </article>
+      </Fragment>
+    );
+  }
+};
+
+export default OwnerInfo;
