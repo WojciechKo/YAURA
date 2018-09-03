4d3d6f62ebada5de9669689aef8a3046bd5333f7
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-09-03 21:25:06 +0200

    update_contracts is now more verbose

--- update_contracts.sh
+++ update_contracts.sh
@@ -2,4 +2,4 @@
 
 beth_address=`truffle migrate --reset | sed -n 's/[[:space:]]*Beth:[[:space:]]*//p'`
 sed -i -e "s/\(REACT_APP_BETH_ADDRESS=\).*/\1${beth_address}/g" '.env.local'
-echo '.env.local updated'
+echo "BethAddres: ${beth_address}"
