3bbf57b9ae9a3e9c720cef868a58aa77951af1d7
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-09-03 17:27:23 +0200

    Allow to place a bet and return total options values

--- contracts/Beth.sol
+++ contracts/Beth.sol
@@ -9,14 +9,26 @@ contract Beth is Ownable {
     string description
   );
 
-  struct Option {
-    bytes32 description;
-    mapping(address => uint) values;
-  }
+  event NewBetOnOption(
+    uint indexed bet_id,
+    uint indexed option_id,
+    uint value
+  );
 
   struct Bet {
     string description;
     Option[] options;
+  }
+
+  struct Option {
+    bytes32 description;
+    uint valuesCount;
+    mapping(uint => OptionValue) values;
+  }
+
+  struct OptionValue {
+    address user;
+    uint amount;
   }
 
   Bet[] public bets;
@@ -30,25 +42,43 @@ contract Beth is Ownable {
 
     uint arrayLength = option_descriptions.length;
     for (uint i=0; i < arrayLength; i++) {
-      bet.options.push(Option(option_descriptions[i]));
+      bet.options.push(Option(option_descriptions[i], 0));
     }
 
     emit NewBet(bet_id, bet.description);
   }
 
-  function getBet(uint index) public constant returns(string, bytes32[]) {
-    Bet memory bet = bets[index];
+  function getBet(uint index) public constant returns(string, bytes32[], uint[]) {
+    Bet storage bet = bets[index];
 
-    uint arrayLength = bet.options.length;
-    bytes32[] memory options = new bytes32[](arrayLength);
+    uint optionsCount = bet.options.length;
+    bytes32[] memory options = new bytes32[](optionsCount);
+    uint[] memory values = new uint[](optionsCount);
 
-    for (uint i=0; i<arrayLength; i++) {
-      options[i] = bet.options[i].description;
+    for (uint i=0; i<optionsCount; i++) {
+      Option storage option = bet.options[i];
+
+      uint value = 0;
+      for (uint j=0; j<option.valuesCount; j++) {
+        value += option.values[j].amount;
+      }
+
+      values[i] = value;
+      options[i] = option.description;
     }
-    return (bet.description, options);
+    return (bet.description, options, values);
   }
 
   function getBetCount() public constant returns(uint) {
     return bets.length;
+  }
+
+  function betOnOption(uint bet_id, uint option_id) public payable {
+    Option storage option = bets[bet_id].options[option_id];
+    uint value_index = option.valuesCount;
+    option.values[value_index] = OptionValue(msg.sender, msg.value);
+    option.valuesCount++;
+
+    emit NewBetOnOption(bet_id, option_id, msg.value);
   }
 }
--- test/js/bethTest.js
+++ test/js/bethTest.js
@@ -82,21 +82,72 @@ contract('Beth', async ([owner, ...accounts]) => {
 
   describe('getBet', async () => {
     const description = 'elo elo';
-    const options = ['tak', 'nie'];
+    const options = ['tak', 'nie', 'nie wiem'];
 
     before(async () => {
+      console.log('create a new bet')
       await Beth.createBet(
         description,
         options
       )
     })
 
-    it('returns description and options of the bet', async () => {
+    it('returns description and options of the bet with values', async () => {
       const betId = parseInt(await Beth.getBetCount()) - 1;
+      //
+      // Make some bets
+      await Beth.betOnOption(betId, 0, {value: 100});
+      await Beth.betOnOption(betId, 0, {value: 50});
+      await Beth.betOnOption(betId, 1, {value: 10000});
+
+      console.log('bet_id', betId);
       const bet = await Beth.getBet(betId);
+
+      // Description
       expect(bet[0]).to.include(description)
+
+      // Options
       expect(web3.toAscii(bet[1][0])).to.include(options[0])
       expect(web3.toAscii(bet[1][1])).to.include(options[1])
+      expect(web3.toAscii(bet[1][2])).to.include(options[2])
+
+      // OptionValues
+      expect(parseInt(bet[2][0])).to.equal(150)
+      expect(parseInt(bet[2][1])).to.equal(10000)
+      expect(parseInt(bet[2][2])).to.equal(0)
+    })
+  })
+
+  describe('betOnOption', async () => {
+    before(async () => {
+      await Beth.createBet(
+        'Kto wygra mecz?',
+        ['Polska', 'Niemcy']
+      )
+    })
+
+    it('places a bet on selected option', async () => {
+      // Given
+      const oldBalance = parseFloat(web3.eth.getBalance(Beth.address))
+      const betId = parseInt(await Beth.getBetCount()) - 1;
+      const optionId = 0;
+      const value = 523;
+
+      // When
+      const response = await Beth.betOnOption(betId, optionId, {value: value});
+      console.log('bet_id', betId);
+
+      // Then emits an Event
+      expect(response.logs).to.have.length(1);
+      const log = response.logs[0];
+
+      expect(log.event).to.equal('NewBetOnOption');
+      expect(parseInt(log.args.bet_id)).to.equal(betId);
+      expect(parseInt(log.args.option_id)).to.equal(optionId);
+      expect(parseInt(log.args.value)).to.equal(value);
+
+      // Then changes the balance of the Contract
+      expect(parseFloat(web3.eth.getBalance(Beth.address))).to.equal(oldBalance + value);
     })
   })
 })
