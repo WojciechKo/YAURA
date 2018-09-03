4de37e487759063ca6eec277a389252b903dae65
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-09-03 17:29:19 +0200

    Some UNIX magic that updates address of deployed contract

---
+++ update_contracts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+beth_address=`truffle migrate --reset | sed -n 's/[[:space:]]*Beth:[[:space:]]*//p'`
+sed -i -e "s/\(REACT_APP_BETH_ADDRESS=\).*/\1${beth_address}/g" '.env.local'
+echo '.env.local updated'
